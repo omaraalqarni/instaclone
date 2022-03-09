7bd3b03bff21d2ee8c51ff1219da3755e192f8fb
omarqr0 <pleess30@gmail.com>
2022-03-10 02:16:50 +0300

    setup api key for the web app. and hid it

--- lib/auth/secrets.dart
+++
@@ -1,7 +0,0 @@
-
-
-String apikey= "AIzaSyD_8BRKSRT8nfUAZPpNJqzw2ARfEoiCFBg";
-String appId= "1:61901777837:web:2c947848f417053ce7cf24";
-String messagingSenderId= "61901777837";
-String projectId= "instaclone-5e6a4";
-String storageBucket= "instaclone-5e6a4.appspot.com";
